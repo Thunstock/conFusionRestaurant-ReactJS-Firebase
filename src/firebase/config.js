68537ec36bab463b997b011c410eca49a53464a4
Tyler Hunstock <Thunstock17@gmail.com>
2019-04-23 21:09:54 -0700

    Finished Project

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+	apiKey: 'AIzaSyASafTTLENc7lhs2wCl2obsC-QTnYO_-No',
+	authDomain: 'confusionserver-70851.firebaseapp.com',
+	databaseURL: 'https://confusionserver-70851.firebaseio.com',
+	projectId: 'confusionserver-70851',
+	storageBucket: 'confusionserver-70851.appspot.com',
+	messagingSenderId: '673256160035'
+};
